b8d34f6cc75da65136f12793ccde42dfe7fe5144
Ryan Pugh <74481946+rpugh10@users.noreply.github.com>
2025-09-22 21:43:31 -0700

    Add files via upload

---
+++ Strings.py
@@ -0,0 +1,9 @@
+def length(input_str):
+    print('Length of that string is', len(input_str))
+
+user_input = input('Enter a string: ')
+print(user_input[2:-2]) #This means start at index 2 and go till index -2 (0 -2).
+length(user_input)
+
+
+   
